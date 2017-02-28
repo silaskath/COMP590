047f2db099e5fd56a1d98a5e8caac079833dee30
Silas Kath <silas.k@mac.com>
2017-02-27 22:15:13 -0500

    Added functions for buttons

--- Assignments/Assignment2/app/src/main/java/com/example/simac/assignment2/MainActivity.java
+++ Assignments/Assignment2/app/src/main/java/com/example/simac/assignment2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.simac.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button accel = (Button) findViewById(R.id.accel_but);
+        Button prox = (Button) findViewById(R.id.prox_but);
+        Button mag = (Button) findViewById(R.id.mag_but);
+    }
+    public void accel(View v){
+        Log.v("TAAG", "Pressed accel.");
+    }
+    public void prox(View v) {
+        Log.v("TAAG", "Pressed prox");
+    }
+    public void mag(View v) {
+        Log.v("TAAG", "Pressed mag");
     }
 }
