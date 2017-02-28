5839f21fd1d0b3d15cccce24b7f20c905d8faa86
Silas Kath <silas.k@mac.com>
2017-02-28 16:55:33 -0500

    Added sample code

--- Assignments/MediaPlayback/app/src/main/java/edu/unc/svkathcs/mediaplayback/MainActivity.java
+++ Assignments/MediaPlayback/app/src/main/java/edu/unc/svkathcs/mediaplayback/MainActivity.java
@@ -3,16 +3,26 @@ package edu.unc.svkathcs.mediaplayback;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     // Two important classses
     // MediaPlayer & AudioPlayer
-
+    private MediaPlayer mp;
     @Override
+    Button play_but = findViewById(play)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this.getApplicationContext(), R.raw.kw_robots);
+//        mp.start();
     }
+
+    public void play(View) { mp.start(); }
+
+//    public void stop(View) { mp.stop(); }
+
     /* ---Playing from local resource---
     MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1);
     mediaPlayer.start(); // good for short sounds (effects)
